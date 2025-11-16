2fde4884fc70c25b5c08bb447b9d3057f287e66f
Sanjit Samar <sanjitsamar07@gmail.com>
2025-11-16 12:13:25 +0000

    added search logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
+import { useState } from "react";
 import "./styles.css";
+import { useEffect } from "react";
 
 export default function App() {
+  const [input, setInput] = useState("");
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const data = await fetch(`https://dummyjson.com/recipes/search?q=${input}`);
+    const result = await data.json();
+    setData(result.recipes);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [input]);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <input
+        className="input-box"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      {data &&
+        data?.map((el) => (
+          <div key={el.id} className="search-container">
+            <div className="search-box">{el.name}</div>
+          </div>
+        ))}
     </div>
   );
 }
